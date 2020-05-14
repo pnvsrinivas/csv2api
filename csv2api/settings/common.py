6ea34dfe9000c0d16536c6892121adefa29f146b
pnvsrinivas <spenke@icubecsi.com>
2020-05-14 23:02:52 +0530

    The STATICFILES_DIRS setting should not contain the STATIC_ROOT setting fix

--- csv2api/settings/common.py
+++ csv2api/settings/common.py
@@ -121,7 +121,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "resources"),
 ]
 
 # print(STATIC_ROOT, MEDIA_ROOT, BASE_DIR)
